a68b99c1bb78fc5d45e6d4e80f4a074fa0b31ac7
Andrii Berlinov <BERLlCH>
2024-02-07 12:22:18 +0200

    Removed old relayer logic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import app from "./app";
 import env from "./env";
 import AppDataSource from "./ormconfig";
-import txStatusChecker from "./worker/txStatusChecker";
-import proofQueue from "./worker/queueWorker";
 import { Account } from "./models/Account";
 import { Proof } from "./models/Proof";
 import { QueueService } from "./worker/queue.service";
-import { Network } from "./services/proof_provider";
 import { SuiProofProvider } from "./services/proof_provider/sui";
 
 const port = env.PORT;
@@ -14,9 +11,6 @@ const port = env.PORT;
 AppDataSource.initialize()
   .then((datasource) => {
     console.log("Data Source has been initialized!");
-
-    // txStatusChecker(datasource).start();
-    // proofQueue(datasource).start();
 
     const accountRepository = datasource.getRepository(Account);
     const proofRepository = datasource.getRepository(Proof);
