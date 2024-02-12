274e6fdbc0fc0d47dac34be3fb00ae3db06d6689
Andrii Berlinov <BERLlCH>
2024-02-12 09:37:29 +0200

    Fixed txChecker issue

--- src/worker/queue.service.ts
+++ src/worker/queue.service.ts
@@ -200,7 +200,7 @@ export class QueueService {
       where: { refId, type: ProofType.AUTHORITY },
     });
 
-    return !!authorityProof.txHash;
+    return !!authorityProof?.txHash;
   }
 
   async isSignatureProofsProcessed(refId: string) {
